aee5ae44a37f71d6a4573dd55b53c85ae65d8cda
XXY <2867811604@qq.com>
2024-09-15 21:50:01 +0800

    清除keil的error

--- Chassis/User/inc/app/main.h
+++ Chassis/User/inc/app/main.h
@@ -10,41 +10,41 @@
 #include <stdlib.h>
 #include "arm_math.h"
 
-///* Mylib */
-//#include "os_tick.h"
-//#include "can2.h"
-//#include "can1.h"
-//#include "counter.h"
-//#include "pc_uart.h"
-//#include "bluetooth.h"
-//#include "remote_control.h"
-//#include "wifi.h"
-//#include "iwdg.h"
-//#include "led.h"
-//#include "i2c.h"
-//#include "bsp_referee.h"
-//#include "bsp_superpower.h"
-//#include "atk_ms53l0m.h"
-//#include "dji_remote.h"
+/* Mylib */
+#include "os_tick.h"
+#include "can2.h"
+#include "can1.h"
+#include "counter.h"
+#include "pc_uart.h"
+#include "bluetooth.h"
+#include "remote_control.h"
+#include "wifi.h"
+#include "iwdg.h"
+#include "led.h"
+#include "i2c.h"
+#include "bsp_referee.h"
+#include "bsp_superpower.h"
+#include "atk_ms53l0m.h"
+#include "dji_remote.h"
 
-///* Algorithm */
+/* Algorithm */
 
-///* Task */
-//#include "Start_Task.h"
-//#include "CPU_Task.h"
-//#include "ChasisControlTask.h"
+/* Task */
+#include "Start_Task.h"
+#include "CPU_Task.h"
+#include "ChasisControlTask.h"
 
-///* Os */
-//#include "FreeRTOSConfig.h"
-//#include "FreeRTOS.h"
-//#include "task.h"
+/* Os */
+#include "FreeRTOSConfig.h"
+#include "FreeRTOS.h"
+#include "task.h"
 
-///* tools */
-//#include "tools.h"
+/* tools */
+#include "tools.h"
 
-///* init */
-//void BSP_Init(void);
-//void Robot_Init(void);
-//void startTask(void);
+/* init */
+void BSP_Init(void);
+void Robot_Init(void);
+void startTask(void);
 
 #endif
